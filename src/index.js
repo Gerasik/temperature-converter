a2c5df3a32444f1175f90c998198fc6d8be3735b
Gerasik <gerasik1992@gmail.com>
2020-02-06 19:55:53 +0300

    submit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 
 exports.cToF = function (c) {
-
+    const result = c * 9/5 + 32;
+    return result.toFixed(2);
 }
 
 exports.fToC = function (f) {
-
+    const result = (f - 32) * 5/9;
+    return result.toFixed(2);
 }
